80d3b8ed0a6885f3a116379e39142f585ad91000
Silke Bonnen <silke.bonnen@gmail.com>
2024-02-27 17:49:20 +0100

    workflow changes

--- test/segment.py
+++ test/segment.py
@@ -2,7 +2,7 @@ from manim import *
 from src.flow import GraphSegment
 
 
-class Graph_Segment(Scene):
+class Segment(Scene):
     def construct(self):
         self.camera.background_color = WHITE
         graphSegment = GraphSegment(np.array([0, 1, 0]), np.array([5, 1, 0]), 5, WHITE)
