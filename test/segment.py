7585bfc813ae15bfce51aee4d0f77523d3da4356
Silke Bonnen <silke.bonnen@gmail.com>
2024-02-27 18:00:45 +0100

    wrong test to see test result

--- test/segment.py
+++ test/segment.py
@@ -5,9 +5,9 @@ from src.flow import GraphSegment
 class Segment(Scene):
     def construct(self):
         self.camera.background_color = WHITE
-        graphSegment = GraphSegment(np.array([0, 1, 0]), np.array([5, 1, 0]), 5, WHITE)
+        graphSegment = GraphSegment(np.array([0, 1, 0]), np.array([6, 1, 0]), 5, WHITE)
         backGroundGraphSegment = GraphSegment(
-            np.array([0, 1, 0]), np.array([5, 1, 0]), 5.2, BLACK
+            np.array([0, 1, 0]), np.array([6, 1, 0]), 5.2, BLACK
         )
 
         self.add(backGroundGraphSegment)
