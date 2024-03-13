828f946f40f58f22d3b3ecc09ebae9747ad39197
rakul <rakulmaria@hotmail.com>
2024-03-13 12:02:21 +0100

    lets go

--- test/Testing_Polygon.py
+++ test/Testing_Polygon.py
@@ -2,7 +2,7 @@ from manim import *
 from src.flow_object import FlowPolygon
 
 
-class Test_Polygon_Example(Scene):
+class Testing_Polygon(Scene):
     def construct(self):
         test = FlowPolygon(6)
         self.add(test)
