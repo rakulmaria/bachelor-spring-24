9ae8215caa0b8921ea5dff05ead7e3dc95833f5b
rakul <rakulmaria@hotmail.com>
2024-03-13 11:28:03 +0100

    big P

--- test/test_polygon.py
+++ test/test_polygon.py
@@ -2,7 +2,7 @@ from manim import *
 from src.flow_object import FlowPolygon
 
 
-class Test_polygon(Scene):
+class Test_Polygon(Scene):
     def construct(self):
         test = FlowPolygon(6)
         self.add(test)
