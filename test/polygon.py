555deede158c66c0667a2173e2dc3b98ffa381e7
rakul <rakulmaria@hotmail.com>
2024-03-13 12:08:27 +0100

    trying old naming convention

--- test/polygon.py
+++ test/polygon.py
@@ -2,7 +2,7 @@ from manim import *
 from src.flow_object import FlowPolygon
 
 
-class Testing_Polygon(Scene):
+class Polygon(Scene):
     def construct(self):
         test = FlowPolygon(6)
         self.add(test)
