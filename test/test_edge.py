8ea863c174074bc7696707dcdb4589c7b15d5494
rakul <rakulmaria@hotmail.com>
2024-04-17 15:47:02 +0200

    tests pass

--- test/test_edge.py
+++ test/test_edge.py
@@ -8,7 +8,7 @@ class Test_Edge(Scene):
     def construct(self):
         self.camera.background_color = WHITE
         vertex0 = Vertex("0", 0, 0)
-        vertex1 = Vertex("1", 4, 0)
+        vertex1 = Vertex("1", 1, 0)
         edge = Edge(vertex0, vertex1, 5)
 
         vertex0.draw()
